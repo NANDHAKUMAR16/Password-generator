fd99de10a155d31236865f3d18053974b96b95af
NANDHAKUMAR BM <138647050+NANDHAKUMAR16@users.noreply.github.com>
2024-03-02 22:33:50 +0530

    main.jsx

--- main.jsx
+++ main.jsx
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./assets/CSS/CurrencyConverter.css";
-import { CurrencyConverter } from './assets/Components/CurrencyConverter.jsx';
+import { PasswordGenerator } from './assets/Components/PasswordGenerator.jsx';
 
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <>
-    <CurrencyConverter />
+    <PasswordGenerator />
   </>
 );
